2474bb2b96f73ee6edcb5cd116bb1be8bcf62c45
badu22 <marko.badurina@gmail.com>
2021-12-17 12:40:52 +0100

    panic attac :)

--- _javascript/main.js
+++ _javascript/main.js
@@ -29,60 +29,63 @@ function showSlides() {
 // Universal variables
 var viewWidth = document.querySelector('body').offsetWidth;
 
-// Animation nadnaslov on scroll
-gsap.from('.ana_tevsic',{
-  scrollTrigger: {
-      trigger: '.ana_tevsic',
-      start: 'top 90%'
-  },
-  x: -viewWidth,
-  opacity: 0,
-  duration: 1,
-  ease:"none"
-})
 
-gsap.from('.maja_kranjec',{
-  scrollTrigger: {
-      trigger: '.maja_kranjec',
-      start: 'top 90%'
-  },
-  x: viewWidth,
-  opacity: 0,
-  duration: 1,
-  ease:"none"
-})
+if (viewWidth > 1024) {
+  // Animation nadnaslov on scroll
+  gsap.from('.ana_tevsic',{
+    scrollTrigger: {
+        trigger: '.ana_tevsic',
+        start: 'top 90%'
+    },
+    x: -viewWidth,
+    opacity: 0,
+    duration: 1,
+    ease:"none"
+  })
+
+  gsap.from('.maja_kranjec',{
+    scrollTrigger: {
+        trigger: '.maja_kranjec',
+        start: 'top 90%'
+    },
+    x: viewWidth,
+    opacity: 0,
+    duration: 1,
+    ease:"none"
+  })
 
 
-gsap.from('.ivana_culjak',{
-  scrollTrigger: {
-      trigger: '.ivana_culjak',
-      start: 'top 90%'
-  },
-  x: -viewWidth,
-  opacity: 0,
-  duration: 1,
-  ease:"none"
-})
-gsap.from('.lidija_tomas',{
-  scrollTrigger: {
-      trigger: '.lidija_tomas',
-      start: 'top 90%'
-  },
-  x: viewWidth,
-  opacity: 0,
-  duration: 1,
-  ease:"none"
-})
-gsap.from('.emilija_lasic',{
-  scrollTrigger: {
-      trigger: '.emilija_lasic',
-      start: 'top 90%'
-  },
-  x: -viewWidth,
-  opacity: 0,
-  duration: 1,
-  ease:"none"
-})
+  gsap.from('.ivana_culjak',{
+    scrollTrigger: {
+        trigger: '.ivana_culjak',
+        start: 'top 90%'
+    },
+    x: -viewWidth,
+    opacity: 0,
+    duration: 1,
+    ease:"none"
+  })
+  gsap.from('.lidija_tomas',{
+    scrollTrigger: {
+        trigger: '.lidija_tomas',
+        start: 'top 90%'
+    },
+    x: viewWidth,
+    opacity: 0,
+    duration: 1,
+    ease:"none"
+  })
+  gsap.from('.emilija_lasic',{
+    scrollTrigger: {
+        trigger: '.emilija_lasic',
+        start: 'top 90%'
+    },
+    x: -viewWidth,
+    opacity: 0,
+    duration: 1,
+    ease:"none"
+  })
+}
 
 
 
